7a0cd6886a7fec02f23ad87a9af1f4e7362b4403
andy-aguilar <aaguil3@gmail.com>
2022-02-13 07:12:56 -0500

    Add new_rule functionality to hashtag setter method

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+
+    def hashtag=(hashtag)
+        # First check if hashtag is actually changing or just getting set to its previous value
+        if(hashtag != self.hashtag)
+            TwitterStream.new_rule(self, hashtag)
+        end
+        # Super allows us to call the standard ActiveRecord setter method after we run the custom code above
+        super(hashtag)
+    end
 end
--- app/models/twitter_stream.rb
+++ app/models/twitter_stream.rb
@@ -1,10 +1,9 @@
-class TwitterSocket < ApplicationRecord
+class TwitterStream < ApplicationRecord
     @@STREAM_URL = "https://api.twitter.com/2/tweets/search/stream"
     @@RULES_URL = "https://api.twitter.com/2/tweets/search/stream/rules"
     @@BEARER_TOKEN = ENV["TWITTER_BEARER_TOKEN"]
     
     def self.stream_connect
-        
         params = {
             "expansions": "attachments.poll_ids,attachments.media_keys,author_id,entities.mentions.username,geo.place_id,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id",
             "tweet.fields": "attachments,author_id,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang",
